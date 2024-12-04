a21621e10e91ae20eb81d85890ae158fd9456d3b
rettr <kirillgvoz@gmail.com>
2024-12-04 20:36:06 +0300

    Added some fitchures, prototype & animation for categories

---
+++ scripts/scripts.js
@@ -0,0 +1,14 @@
+// categories div opening script
+const container = document.getElementById("categories-content");
+const button = document.getElementById("toggleButton");
+button.addEventListener("click", () => {
+container.classList.toggle("open");
+});
+
+// email sending feedback script
+const email_button = document.getElementById("send-email-button");
+const email_fill = document.getElementById("email-fill");
+const message = document.getElementById("message");
+email_button.addEventListener("click", function() {
+    message.textContent = "Спасибо! Подписка оформлена";
+});
