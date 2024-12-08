6d9fbb7ae39a97796869bc90ca9b6a6361959c28
rettr <kirillgvoz@gmail.com>
2024-12-08 23:56:41 +0300

    added lists realistion

---
+++ scripts/catalogue.js
@@ -0,0 +1,32 @@
+const productList = document.getElementById("product-list");
+const products = [
+    {image: "../images/cards_categories/Vegetables_Cucumbers.jpg", title:"Товар 1", description: "Описание товара 1", price: 53, avialiblenes: true},
+    {image: "../images/cards_categories/Vegetables_Cucumbers.jpg", title:"Товар 2", description: "Описание товара 2", price: 30, avialiblenes: false},
+    {image: "../images/cards_categories/Vegetables_Cucumbers.jpg", title:"Товар 3", description: "Описание товара 3", price: 44, avialiblenes: true }
+  ];
+
+function createProductCard(product) {
+    let avialible = product.avialiblenes == true ? "Есть в наличии" : "Нет в наличии";
+    return `
+      <div class="goods-card">
+            <div class="img-container goods-img-container">
+                <img class = "category-img goods-img" src="${product.image}" alt="Фото товара">
+            </div>
+            <div class = "goods-text-container">
+                <p class = "goods-name">${product.title}</p>
+                <div class = "goods-description-n-indicator">
+                    <p class = "goods-description">${product.description}
+                    </p>
+                    <div class = "goods-indicator">
+                        <p class = "goods-indicator-text"> Цена: ${product.price}Р</p>
+                        <p class = "goods-indicator-text ${product.avialiblenes == true ? "availible-good" : ""}">${avialible}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+  }
+
+  products.forEach(product => {
+    productList.innerHTML += createProductCard(product);
+  });
