7800931b47582476b63fcccfea263da3c1509f2c
kirillgvoz <136619340+kirillgvoz@users.noreply.github.com>
2024-12-09 17:12:01 +0300

    tried to fix catalogue photos

--- scripts/catalogue.js
+++ scripts/catalogue.js
@@ -1,8 +1,8 @@
 const productList = document.getElementById("product-list");
 const products = [
-    {image: "../images/catalogue/cucmbers_ctlg.jpg", title:"Огурцы от Петровны", description: "Супер крутые огуречики, лечат всё на свете - хирургическое образование, две учёные степени", price: 53, avialiblenes: true},
-    {image: "../images/catalogue/tomatoes_ctlg.jpeg", title:"Сеньоры помидоры, здоровые как боинг", description: "Поступают жалобы, что соседи по огороду путают их с арбузами", price: 30, avialiblenes: false},
-    {image: "../images/catalogue/bolgar_ctlg.jpg", title:"Перцы болгарские, но без акцента", description: "Не важно, какой перец - красный или жёлтый, ведь оба окажутся в салате", price: 44, avialiblenes: true }
+    {image: "/../images/catalogue/cucmbers_ctlg.jpg", title:"Огурцы от Петровны", description: "Супер крутые огуречики, лечат всё на свете - хирургическое образование, две учёные степени", price: 53, avialiblenes: true},
+    {image: "/../images/catalogue/tomatoes_ctlg.jpeg", title:"Сеньоры помидоры, здоровые как боинг", description: "Поступают жалобы, что соседи по огороду путают их с арбузами", price: 30, avialiblenes: false},
+    {image: "/../images/catalogue/bolgar_ctlg.jpg", title:"Перцы болгарские, но без акцента", description: "Не важно, какой перец - красный или жёлтый, ведь оба окажутся в салате", price: 44, avialiblenes: true }
   ];
 
 function createProductCard(product) {
