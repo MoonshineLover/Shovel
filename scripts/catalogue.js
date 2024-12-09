fad5156e49ab8d206dc10db93f8f598591b509b2
rettr <kirillgvoz@gmail.com>
2024-12-09 14:05:29 +0300

    bug fixes

--- scripts/catalogue.js
+++ scripts/catalogue.js
@@ -1,8 +1,8 @@
 const productList = document.getElementById("product-list");
 const products = [
-    {image: "../images/catalogue/Vegetables_Cucumbers.jpg", title:"Товар 1", description: "Описание товара 1", price: 53, avialiblenes: true},
-    {image: "../images/catalogue/Vegetables_Cucumbers.jpg", title:"Товар 2", description: "Описание товара 2", price: 30, avialiblenes: false},
-    {image: "../images/catalogue/Vegetables_Cucumbers.jpg", title:"Товар 3", description: "Описание товара 3", price: 44, avialiblenes: true }
+    {image: "../images/catalogue/cucmbers_ctlg.jpg", title:"Огурцы от Петровны", description: "Супер крутые огуречики, лечат всё на свете - хирургическое образование, две учёные степени", price: 53, avialiblenes: true},
+    {image: "../images/catalogue/tomatoes_ctlg.jpeg", title:"Сеньоры помидоры, здоровые как боинг", description: "Поступают жалобы, что соседи по огороду путают их с арбузами", price: 30, avialiblenes: false},
+    {image: "../images/catalogue/bolgar_ctlg.jpg", title:"Перцы болгарские, но без акцента", description: "Не важно, какой перец - красный или жёлтый, ведь оба окажутся в салате", price: 44, avialiblenes: true }
   ];
 
 function createProductCard(product) {
@@ -18,7 +18,7 @@ function createProductCard(product) {
                     <p class = "goods-description">${product.description}
                     </p>
                     <div class = "goods-indicator">
-                        <p class = "goods-indicator-text"> Цена: ${product.price}Р</p>
+                        <p class = "goods-indicator-text goods-price"> Цена: ${product.price}Р</p>
                         <p class = "goods-indicator-text ${product.avialiblenes == true ? "availible-good" : ""}">${avialible}</p>
                     </div>
                 </div>
